8710ee716fad4aef2c5b0d6cd4b3ae023dabb417
yap cheah shen <yapcheahshen@gmail.com>
2014-01-14 00:49:53 +0800

    all done except s12

--- parsesltp.js
+++ parsesltp.js
@@ -4,25 +4,40 @@ outputfolder='xml/';
 lst=fs.readFileSync('./sltp.lst','utf8').replace(/\r\n/g,'\n').split('\n');
 titles=require('./titles.json');
 titlenow=0;
-
+prevfile="";
+var out=[];
 var newfile=function(fn) {
-	console.log('create file',fn)
+	if (prevfile) {
+		console.log('saving file',prevfile)
+		fs.writeFileSync( outputfolder+prevfile+'.xml',out.join('\n'),'utf8')
+		out=[];
+	}
+	if (!fn) return;
+	prevfile=fn;
 }
-
+/*
+{"id":"s12","caption":"nidānasaṃyuttaṃ"},
+*/
 var dofile=function(f) {
 	//console.log('processing',f)
 	var arr=fs.readFileSync(sourcefolder+f,'utf8').replace(/\r\n/g,'\n').split('\n');
-	var out=[];
+
 	var lastpid=0,i=0;
 	while(i<arr.length) {
-		title=titles[titlenow].caption;
-		if (arr[i].toLowerCase().indexOf(title)>-1) {
-			newfile(titles[titlenow].id);
-			titlenow++;
+		if (titlenow<titles.length) {
+			title=titles[titlenow].caption;
+			if (arr[i].toLowerCase().indexOf(title)>-1) {
+				newfile(titles[titlenow].id);
+				titlenow++;
+			}			
 		}
+
+		out.push(arr[i]);
 		i++;
 	}
+	
 }
 
 lst.map(function(file){dofile(file)});
 
+newfile();
